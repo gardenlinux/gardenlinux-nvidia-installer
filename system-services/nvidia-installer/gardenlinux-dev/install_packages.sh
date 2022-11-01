d2fdacd257f8a6cb4d4dec5d9d6fdb41e3b63ca7
Darren Hague <d.hague@sap.com>
2022-11-01 08:52:59 +0000

    nvidia-installer: improve resilience to flaky snapshot.debian.org (#5118)

--- system-services/nvidia-installer/gardenlinux-dev/install_packages.sh
+++ system-services/nvidia-installer/gardenlinux-dev/install_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -19,23 +19,38 @@ if [ -z $GCC_LINUX_VERSION ]; then
   GCC_LINUX_VERSION=${LINUX_VERSION}
 fi
 
+#DATESTAMP=$(echo ${LINUX_DATE} | sed 's/-//g') && \
 DATESTAMP=$(echo ${LINUX_DATE} | sed 's/-//g') && rm -f /etc/apt/sources.list && \
   echo "deb http://snapshot.debian.org/archive/debian/${DATESTAMP}T000000Z testing main" >> /etc/apt/sources.list
 
 apt-get -o Acquire::Check-Valid-Until=false update
-GCC_MAJOR=$(echo $GCC_VERSION | cut -d '.' -f1) && \
-    apt-get install -y --allow-downgrades --no-install-recommends \
-      gcc-${GCC_MAJOR}-base=${GCC_VERSION} \
-      cpp-${GCC_MAJOR}=${GCC_VERSION} \
-      gcc-${GCC_MAJOR}=${GCC_VERSION} \
-      libgcc-${GCC_MAJOR}-dev=${GCC_VERSION} \
-      build-essential \
-      ca-certificates \
-      curl \
-      kmod \
-      libelf-dev \
-      binutils \
-      wget
+GCC_MAJOR=$(echo $GCC_VERSION | cut -d '.' -f1)
+
+packages=("build-essential" \
+      "gcc-${GCC_MAJOR}-base=${GCC_VERSION}" \
+      "cpp-${GCC_MAJOR}=${GCC_VERSION}" \
+      "gcc-${GCC_MAJOR}=${GCC_VERSION}" \
+      "libgcc-${GCC_MAJOR}-dev=${GCC_VERSION}" \
+      "ca-certificates" \
+      "curl" \
+      "kmod" \
+      "libelf-dev" \
+      "binutils" \
+      "wget")
+
+for package in "${packages[@]}"; do
+  max_retry=5
+  counter=0
+  until apt-get install -y --allow-downgrades --no-install-recommends $package
+  do
+     sleep 1
+     [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
+     echo "Trying again. Try #$counter"
+     ((counter++)) || true
+  done
+done
+
+
 apt autoremove -y
 
 # Add packages from private gardenlinux source
