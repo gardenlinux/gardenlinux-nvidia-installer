bc5bfb6fa278295cf43f49c2922d07758c618192
Darren Hague <d.hague@sap.com>
2023-12-05 15:44:45 +0000

    More tidying

--- resources/set_env_vars.sh
+++ resources/set_env_vars.sh
@@ -8,3 +8,7 @@ export DRIVER_NAME=nvidia
 export DRIVER_VERSION=$(ls /out/nvidia | sed 's/.tar.gz//')
 export NVIDIA_ROOT="${CACHE_DIR}/${DRIVER_NAME}/${DRIVER_VERSION}"
 export LD_LIBRARY_PATH="${NVIDIA_ROOT}/lib"
+
+declare -A arch_translation
+arch_translation=(["amd64"]="x86_64" ["arm64"]="aarch64")
+ARCH_TYPE=${arch_translation[$TARGET_ARCH]}
