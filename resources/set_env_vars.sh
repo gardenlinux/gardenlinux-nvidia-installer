1a351e6eb58f01e5aae52104b7a067e55df09969
Darren Hague <d.hague@sap.com>
2023-12-11 15:45:19 +0000

    More Shell script style improvements (#10)

--- resources/set_env_vars.sh
+++ resources/set_env_vars.sh
@@ -10,9 +10,10 @@ export NVIDIA_ROOT="${CACHE_DIR}/${DRIVER_NAME}/${DRIVER_VERSION}"
 export LD_LIBRARY_PATH="${NVIDIA_ROOT}/lib"
 
 if [ -z "$TARGET_ARCH" ]; then
-  export ARCH_TYPE=$(uname -m)
+  ARCH_TYPE=$(uname -m)
 else
   declare -A arch_translation
   arch_translation=(["amd64"]="x86_64" ["arm64"]="aarch64")
-  export ARCH_TYPE=${arch_translation[$TARGET_ARCH]}
+  ARCH_TYPE=${arch_translation[$TARGET_ARCH]}
 fi
+export ARCH_TYPE
