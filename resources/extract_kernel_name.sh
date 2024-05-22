594890de9968173bec85575f764c7f27d267fed0
Darren Hague <d.hague@sap.com>
2024-05-22 17:50:55 +0100

    shellheck fix

--- resources/extract_kernel_name.sh
+++ resources/extract_kernel_name.sh
@@ -12,9 +12,9 @@ else
 fi
 
 kernel_arch=$2
-#                List the linux-headers folders for the arch & kernel type -------------- | Sort by line length (shortest first) ---------------- | Pick the first line
 # shellcheck disable=SC2010,SC2086
-kernel_headers=$(ls -d /usr/src/linux-headers-* | grep "${kernel_arch}" | grep $grep_args | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- | head -n 1)
+#                List the linux-headers folders for the arch & kernel type ------------------- | Sort by line length (shortest first) ---------------- | Pick the first line
+kernel_headers=$(ls /usr/src | grep "linux-headers-" | grep "${kernel_arch}" | grep $grep_args | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- | head -n 1)
 
 kernel_name=${kernel_headers//linux-headers-/}
 
