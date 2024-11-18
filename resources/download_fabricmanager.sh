bc528c96b328975398e1271fbd35616dfe32a71c
Janos Binder <janos.binder@sap.com>
2024-11-18 15:07:53 +0100

    fix: address PR comments

--- resources/download_fabricmanager.sh
+++ resources/download_fabricmanager.sh
@@ -25,6 +25,5 @@ pushd /tmp/nvidia
 # Download Fabric Manager tarball
 wget -O /tmp/keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i /tmp/keyring.deb
 apt-get update
-#apt-get install -V nvidia-open-"$DRIVER_BRANCH"
-apt-get install -V nvidia-fabricmanager-"$DRIVER_BRANCH" nvidia-fabricmanager-dev-"$DRIVER_BRANCH"
+apt-get install -V nvidia-fabricmanager-"$DRIVER_BRANCH"
 
