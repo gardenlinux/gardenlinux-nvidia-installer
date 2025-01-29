5708d7e5a7b5d3642465f958e577ee8f0ceaea71
Janos Binder <janosbinder@users.noreply.github.com>
2025-01-29 14:52:22 +0100

    fix: download_fabricmanager.sh - make binding to the driver version (#39)

--- resources/download_fabricmanager.sh
+++ resources/download_fabricmanager.sh
@@ -23,5 +23,5 @@ pushd /tmp/nvidia
 # Download Fabric Manager tarball
 wget -O /tmp/keyring.deb https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb && dpkg -i /tmp/keyring.deb
 apt-get update
-apt-get install -V nvidia-fabricmanager-"$DRIVER_BRANCH"
+apt-get install -V nvidia-fabricmanager-"$DRIVER_BRANCH"="$DRIVER_VERSION"-1
 
