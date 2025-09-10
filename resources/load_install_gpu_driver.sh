aca21ad068e42c9f34274089120ed205256124f8
Darren Hague <d.hague@sap.com>
2025-09-10 19:32:14 +0100

    Fix nvidia-modprobe #3

--- resources/load_install_gpu_driver.sh
+++ resources/load_install_gpu_driver.sh
@@ -6,6 +6,7 @@ BIN_DIR=${BIN_DIR:-/opt/nvidia-installer}
 # shellcheck disable=SC1090
 source "$BIN_DIR"/set_env_vars.sh
 LD_ROOT=${LD_ROOT:-/root}
+NVIDIA_ROOT=${NVIDIA_ROOT:-/run/nvidia/driver}
 
 main() {
     # Populate DRIVER_NAME, DRIVER_VERSION, NVIDIA_ROOT, etc.
