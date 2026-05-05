9656afa367d77ae1df5b419d338c1aa15337b8ba
Sujana Subramaniam <sujana.subramaniam@sap.com>
2026-05-05 10:10:37 +0200

    cdup improvement

--- cdup/main.go
+++ cdup/main.go
@@ -209,10 +209,10 @@ func run(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("failed to set ocm configuration: %w", err)
 	}
 
-	//err = o.PublishComponentDescriptor(context.Background(), version, y)
-	//if err != nil {
-	//	return fmt.Errorf("component descriptor publish failed: %w", err)
-	//}
+	err = o.PublishComponentDescriptor(context.Background(), version, y)
+	if err != nil {
+		return fmt.Errorf("component descriptor publish failed: %w", err)
+	}
 
 	o = &oci{}
 
@@ -251,10 +251,10 @@ func run(cmd *cobra.Command, args []string) error {
 	}
 	fmt.Println(string(yml))
 
-	//err = o.PublishComponentDescriptor(context.Background(), newVersion, yml)
-	//if err != nil {
-	//	return fmt.Errorf("component descriptor publish failed: %w", err)
-	//}
+	err = o.PublishComponentDescriptor(context.Background(), newVersion, yml)
+	if err != nil {
+		return fmt.Errorf("component descriptor publish failed: %w", err)
+	}
 
 	err = o.Close()
 	if err != nil {
