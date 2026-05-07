5a305996019aca7e8e36cabba9c27a95727605bb
Sujana Subramaniam <sujana.subramaniam@sap.com>
2026-05-07 10:15:51 +0200

    TARGET_ARCH removed from download_fabric_manager

--- resources/download_fabricmanager.sh
+++ resources/download_fabricmanager.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 echo "Downloading NVIDIA fabric manager for driver version $DRIVER_VERSION"
 DRIVER_BRANCH=$(echo "$DRIVER_VERSION" | grep -oE '^[0-9]+')
-if [ -z "$TARGET_ARCH" ]; then
-    echo "Error: TARGET_ARCH is not set."
-    exit 1
-fi
-
-declare -A arch_translation
-arch_translation=(["amd64"]="x86_64" ["arm64"]="aarch64")
-
-if [[ ! ${arch_translation[$TARGET_ARCH]+_} ]]; then
-    echo "Error: Unsupported TARGET_ARCH value."
-    exit 2
-fi
 
 mkdir -p /tmp/nvidia
 
