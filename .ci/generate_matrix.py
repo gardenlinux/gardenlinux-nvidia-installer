2a9a22b7808c3d1488a0f4ab84d628437c0395f9
Darren Hague <d.hague@sap.com>
2026-07-03 09:22:02 +0100

    Revert "Add gVisor build path with nvproxy-qualified driver pinning (#304)" (#310)
    This reverts commit 950b959aa8ec44d7650767839e9bd5c2b509dbef.

--- .ci/generate_matrix.py
+++ .ci/generate_matrix.py
@@ -2,7 +2,6 @@
 import itertools
 import yaml
 import json
-import sys
 
 with open("versions.yaml") as f:
     data = yaml.safe_load(f)
@@ -40,54 +39,11 @@ manifest_matrix = [
     )
 ]
 
-# gvisor_build_matrix: same dimensions as build_matrix but uses pinned driver
-# versions from gvisor_driver_pins and marks entries with image_subfolder=gvisor.
-gvisor_pins = data.get("gvisor_driver_pins", {})
-for driver in data["nvidia_drivers"]:
-    if driver not in gvisor_pins:
-        print(
-            f"Error: nvidia_drivers entry '{driver}' has no entry in gvisor_driver_pins",
-            file=sys.stderr,
-        )
-        sys.exit(1)
-
-gvisor_build_matrix = [
-    {
-        "os_version": os_version,
-        "driver_version": gvisor_pins[driver],
-        "arch": arch,
-        "kernel_flavour": flavour,
-        "image_subfolder": "gvisor",
-    }
-    for os_version, arch, flavour, driver in itertools.product(
-        data["os_versions"],
-        data["cpu_arch"],
-        data["kernel_flavour"],
-        data["nvidia_drivers"],
-    )
-]
-
-gvisor_manifest_matrix = [
-    {
-        "os_version": os_version,
-        "driver_version": gvisor_pins[driver],
-        "kernel_flavour": flavour,
-        "image_subfolder": "gvisor",
-    }
-    for os_version, flavour, driver in itertools.product(
-        data["os_versions"],
-        data["kernel_flavour"],
-        data["nvidia_drivers"],
-    )
-]
-
 print(
     json.dumps(
         {
             "build": {"include": build_matrix},
             "manifest": {"include": manifest_matrix},
-            "gvisor_build": {"include": gvisor_build_matrix},
-            "gvisor_manifest": {"include": gvisor_manifest_matrix},
         }
     )
 )
