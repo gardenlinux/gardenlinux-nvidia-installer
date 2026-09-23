5de30410005f7257f56ea58e72fb8a5f4771ffa3
Moritz Kirchner <moritz.kirchner@sap.com>
2026-09-23 13:50:07 +0200

    Remove 570 driver version as it doesn't compile with either 1877 or 2150
    and also remove 1592 as it is deprecated by gardener
    
    Signed-off-by: Moritz Kirchner <moritz.kirchner@sap.com>

--- .ci/update_versions.py
+++ .ci/update_versions.py
@@ -169,7 +169,7 @@ def get_latest_gardenlinux_tags(data):
             else:  # Stop when we get a match, so we avoid also removing new versions at the top
                 break
 
-    filtered_os_versions = [x for x in new_os_versions if x not in ancient_versions] + ['1592.18'] # remove 1592.18 after gardener stops supporting it 2026-09
+    filtered_os_versions = [x for x in new_os_versions if x not in ancient_versions]
 
     if sorted(data['os_versions']) != sorted(filtered_os_versions):
         data['os_versions'] = filtered_os_versions
